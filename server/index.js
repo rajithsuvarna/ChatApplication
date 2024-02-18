47e97cdc798b1cfa52d5dbdb8933539c4aac2f73
rajithsuvarna <rajithsuvarna49@gmail.com>
2024-02-18 14:45:50 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
+//initialising express
 const express = require("express");
 const app = express();
+//initialising http,cors
 const http = require("http");
 const cors = require("cors");
+//using socket.io
 const { Server } = require("socket.io");
 app.use(cors());
 
+//setting up express server
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -14,23 +18,25 @@ const io = new Server(server, {
   },
 });
 
+//setting up socket connection
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
-
+  //join room
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
-
+  //send message
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   });
-
+  //disconnect
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port:3000");
+//running the server
+server.listen(3001, () => {
+  console.log("Server running on port:8000");
 });
